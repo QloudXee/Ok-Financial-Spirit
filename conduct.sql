1fca72c2ebc2a99c2221cb2afbcdcae2e2f32557
QlouXee <781058829@qq.com>
2017-10-18 19:09:33 +0800

    更新了sql文件

--- conduct.sql
+++ conduct.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50719
 File Encoding         : 65001
 
-Date: 2017-10-16 11:29:47
+Date: 2017-10-18 10:30:30
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -24,6 +24,7 @@ CREATE TABLE `tb_import` (
   `inputNum` int(11) DEFAULT NULL,
   `userId` bigint(20) NOT NULL,
   `statues` int(11) NOT NULL DEFAULT '1',
+  `date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `touser` (`userId`),
   CONSTRAINT `touser` FOREIGN KEY (`userId`) REFERENCES `tb_user` (`id`)
@@ -42,6 +43,7 @@ CREATE TABLE `tb_output` (
   `outputNum` int(11) DEFAULT NULL,
   `userId` bigint(20) NOT NULL,
   `statues` int(11) NOT NULL DEFAULT '1',
+  `date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `user` (`userId`),
   CONSTRAINT `user` FOREIGN KEY (`userId`) REFERENCES `tb_user` (`id`)
@@ -64,7 +66,7 @@ CREATE TABLE `tb_user` (
   `email` varchar(30) DEFAULT NULL,
   `sex` varchar(5) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of tb_user
@@ -80,3 +82,14 @@ INSERT INTO `tb_user` VALUES ('18', '刘颖', '111', '0', '1', 'ly@qq.com', '女
 INSERT INTO `tb_user` VALUES ('20', 'qloud', '123', '0', '1', '123@qloud.com', '男');
 INSERT INTO `tb_user` VALUES ('24', '啦啦啦', 'lalala', '0', '1', 'lalala@lalala.com', '男');
 INSERT INTO `tb_user` VALUES ('25', '哎嘿嘿', 'ahh1', '0', '1', 'ahh@ahh.com', '女');
+INSERT INTO `tb_user` VALUES ('26', 'xxx', 'xxx', '0', '0', 'xx@xx.com', '男');
+INSERT INTO `tb_user` VALUES ('27', '第一组', 'diyizu', '0', '1', 'diyi@diyi.com', '女');
+INSERT INTO `tb_user` VALUES ('28', '甜妞', 'ty', '0', '1', 'tianniu@qq.com', '女');
+INSERT INTO `tb_user` VALUES ('29', '每林', 'ml', '0', '1', 'ml@qq.com', '女');
+INSERT INTO `tb_user` VALUES ('30', '撸神', 'ls', '0', '1', 'ls@qq.com', '男');
+INSERT INTO `tb_user` VALUES ('31', '师傅', 'sf', '0', '1', 'sf@qq.com', '男');
+INSERT INTO `tb_user` VALUES ('32', '队长', 'dz', '0', '1', 'dz@qq.com', '男');
+INSERT INTO `tb_user` VALUES ('33', '瘦鸟', 'sn', '0', '1', 'sn@qq.com', '男');
+INSERT INTO `tb_user` VALUES ('34', '煞笔', 'sb', '0', '1', 'sb@qq.com', '男');
+INSERT INTO `tb_user` VALUES ('35', 'bb', 'bb', '0', '1', 'bb@qq.com', '女');
+INSERT INTO `tb_user` VALUES ('36', 'cc', 'cc', '0', '1', 'cc@qq.com', '女');
